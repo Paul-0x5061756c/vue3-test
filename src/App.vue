025c1d20bcfb6747a57a304a2d6f3826a1142ad0
Paul den Boer <paul@levarne.nl>
2022-01-20 22:11:21 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+import Test from "./components/TestComponent.vue";
+
+
+//data
+var todos: Array<Object> = reactive([])
+var msg = "Hi, we are here to test TS / Vue3 & Composition Api / Vite 🚀"
+
+function addToList(val: Object) {
+  todos.push(val)
+}
+</script>
+
+<template>
+	<Test :msg="msg" :toDos="todos" @add="addToList" />
+</template>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+
+interface toDoObject {
+  title?: string,
+  item?: string
+}
+
+defineProps<{
+  msg?: string,
+  toDos: Array<toDoObject>
+}>()
+
+const emitObject = <toDoObject>{}
+
+</script>
+
+<template lang="pug">
+.wrapper
+  h1 {{ msg }}
+  br
+  input(type="text" placeholder="Title" v-model='emitObject.title')
+  input(type="text" placeholder="Item" v-model='emitObject.item')
+
+  button(@click='$emit("add", emitObject )') Add Todo 🔥
+  .todo-list
+    .list(v-for="todo in toDos")
+      span {{ todo.title }}
+      p {{ todo.item }}
+
+</template>
+
+<style scoped>
+a {
+	color: #42b983;
+}
+
+label {
+	margin: 0 0.5em;
+	font-weight: bold;
+}
+
+code {
+	background-color: #eee;
+	padding: 2px 4px;
+	border-radius: 4px;
+	color: #304455;
+}
+</style>
